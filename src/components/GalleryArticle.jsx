76cb3c16a301a1ae8fdef3e131c0b735d355b548
saifullah-10 <saifullah.bus@gmail.com>
2024-05-13 07:39:07 +0600

    modified gallery page

--- src/components/GalleryArticle.jsx
+++ src/components/GalleryArticle.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "propTypes";
 export default function GalleryArticle({ data }) {
   const { photo_URL, user_feedback, user_name } = data || {};
   return (
@@ -8,21 +9,26 @@ export default function GalleryArticle({ data }) {
           alt=""
           className="absolute inset-0 object-cover w-full h-full -z-10"
         />
-        <div className="absolute inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40"></div>
-        <div className="absolute inset-0 -z-10 rounded-xl ring-1 ring-inset ring-gray-900/10"></div>
-        <div
-          className="text-lg my-4 font-semibold leading-6 text-white hover:text-teal-100"
-          href=""
-        >
-          <span>Name: </span> {user_name}
-        </div>
-        <div
-          className="text-lg font-semibold leading-6 text-white hover:text-teal-100"
-          href=""
-        >
-          <span>Feedback: </span> <h3>{user_feedback}</h3>
+
+        <div className="absolute flex opacity-0  transition hover:opacity-100 inset-0 -z-10 bg-gradient-to-t from-gray-900 via-gray-900/40">
+          <div className="  self-end p-5 ">
+            <div
+              className="text-lg my-4 font-semibold leading-6 text-[#f5b388]"
+              href=""
+            >
+              <span>Name: </span> {user_name}
+            </div>
+            <div className="text-lg font-semibold leading-6 text-[#f5b388]">
+              <span className=" text-[#F9985D]">Feedback: </span>{" "}
+              <h3>{user_feedback}</h3>
+            </div>
+          </div>
         </div>
       </article>
     </>
   );
 }
+
+GalleryArticle.propTypes = {
+  data: PropTypes.object.isRequired,
+};
